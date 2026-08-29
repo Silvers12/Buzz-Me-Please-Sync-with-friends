4bac020316e573b698d2d669e394ae28326e92ce
Silvers <Silvers-V@hotmail.fr>
2026-08-29 20:23:53 +0200

    L'identifiant du paquet passe en minuscules
    Le Play Console attend com.osala.buzzmeplease : c'est sous ce nom que
    l'application y est enregistrée. Seul applicationId change ; le paquet
    des sources reste com.osala.BuzzMePlease, invisible pour le magasin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.osala.BuzzMePlease"
+        applicationId = "com.osala.buzzmeplease"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
