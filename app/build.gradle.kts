ff2fe7837507befb5a783fb74a073f92c5d88602
Silvers <Silvers-V@hotmail.fr>
2026-08-30 21:05:09 +0200

    Version 1.08

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.osala.buzzmeplease"
         minSdk = 26
         targetSdk = 36
-        versionCode = 107
-        versionName = "1.07"
+        versionCode = 108
+        versionName = "1.08"
     }
 
     buildTypes {
